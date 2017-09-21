ebd0240ecd32a1cdbc32cc697fefdd9262f52394
Logan James Kaser <lkaser@e1z1r3p42.42.us.org>
2017-09-21 15:15:14 -0700

    FIX: More norme bullshit.

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: lkaser <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/18 15:01:52 by lkaser            #+#    #+#             */
-/*   Updated: 2017/09/20 17:14:31 by lkaser           ###   ########.fr       */
+/*   Updated: 2017/09/21 15:13:20 by lkaser           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 #include <stdio.h>
 #include <strings.h>
 
-void	putstr(char *str)
+static void	putstr(char *str)
 {
 	while (*str)
 		write(1, str++, 1);
 }
 
-void	putstrln(char *str)
+static void	putstrln(char *str)
 {
 	putstr(str);
 	write(1, "\n", 1);
 }
 
-void	show(char *varname, void *var, size_t bytes)
+static void	show(char *varname, void *var, size_t bytes)
 {
 	unsigned	i;
 	char		*str;
@@ -46,16 +46,21 @@ void	show(char *varname, void *var, size_t bytes)
 	write(1, "\"\n", 2);
 }
 
-void	plus2(char *c)
+static void	plus2(char *c)
 {
 	*c += 2;
 }
 
-int		main(void)
+int			main(void)
 {
-	char *mem;
-	char *src = "apple";
+	char	*mem;
+	char	*src;
 
+	src[0] = 'a';
+	src[1] = 'p';
+	src[2] = 'p';
+	src[3] = 'l';
+	src[4] = 'e';
 	putstrln("\n** ft_memset **\n");
 	mem = (char *)malloc(7);
 	putstrln("mem is 7 bytes..");
@@ -66,12 +71,10 @@ int		main(void)
 	putstrln("Calling ft_memset(str, 'z', 3)");
 	ft_memset(mem, 'z', 3);
 	show("mem", mem, 7);
-
 	printf("\n** ft_isalpha **\n\n");
 	printf("ft_isalpha('a') = %i\n", ft_isalpha('a'));
 	printf("ft_isalpha('1') = %i\n", ft_isalpha('1'));
 	printf("ft_isalpha('\\0') = %i\n", ft_isalpha('\0'));
-
 	putstrln("\n** ft_bzero **\n");
 	show("mem", mem, 7);
 	putstrln("Calling ft_bzero(str, 2)");
@@ -80,14 +83,12 @@ int		main(void)
 	putstrln("Calling ft_bzero(str, 7)");
 	ft_bzero(mem, 7);
 	show("mem", mem, 7);
-
 	putstrln("\n** ft_memcpy **\n");
 	show("mem", mem, 7);
 	show("str", src, 5);
 	putstrln("Calling ft_memcpy(mem, src, 5)");
 	ft_memcpy(mem, src, 5);
 	show("mem", mem, 7);
-
 	memset(mem, 0, 7);
 	putstrln("\n** ft_memccpy **\n");
 	show("mem", mem, 7);
@@ -95,7 +96,6 @@ int		main(void)
 	putstrln("Calling ft_memccpy(mem, src, 5)");
 	ft_memccpy(mem, src, 'l', 5);
 	show("mem", mem, 7);
-
 	memset(mem, 0, 7);
 	putstrln("\n** ft_memmov **\n");
 	show("mem", mem, 7);
@@ -103,7 +103,6 @@ int		main(void)
 	putstrln("Calling ft_memmov(mem, src, 5)");
 	ft_memmove(mem, src, 5);
 	show("mem", mem, 7);
-
 	ft_striter(mem, plus2);
 	show("mem", mem, 5);
 	return (0);
